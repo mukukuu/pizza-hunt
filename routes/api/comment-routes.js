e77283c962036395cdef7d21bb8c6085a48bec9a
mukukuu <qinii@me.com>
2022-03-08 00:48:33 -0700

    completed

---
+++ routes/api/comment-routes.js
@@ -0,0 +1,24 @@
+const router = require('express').Router();
+const { addComment, 
+        removeComment,
+        addReply,
+        removeReply
+     } = require('../../controllers/comment-controller');
+
+
+
+// /api/comments/<pizzaId>
+router.route('/:pizzaId').post(addComment);
+
+// /api/comments/<pizzaId>/<commentId>
+router
+  .route('/:pizzaId/:commentId')
+  .put(addReply)
+  .delete(removeComment);
+
+//delete route
+router.route('/:pizzaId/:commentId/:replyId').delete(removeReply);
+
+
+
+module.exports = router;
